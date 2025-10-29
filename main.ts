87813da42d1a20966a619464f8fe29b48628ddbb
tebssiba-star <tebssiba@gmail.com>
2025-10-29 21:49:33 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    Piano.TP_PlayMusic(262, music.beat(BeatFraction.Whole))
+    Piano.TP_PlayMusic(698, music.beat(BeatFraction.Whole))
+    Piano.TP_PlayMusic(698, music.beat(BeatFraction.Whole))
+    Piano.TP_PlayMusic(880, music.beat(BeatFraction.Whole))
+    Piano.TP_PlayMusic(523, music.beat(BeatFraction.Whole))
+    Piano.TP_PlayMusic(880, music.beat(BeatFraction.Whole))
+    Piano.TP_PlayMusic(349, music.beat(BeatFraction.Whole))
+})
